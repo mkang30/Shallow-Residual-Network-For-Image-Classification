f72da39e67576da0469d467a0f5a2e0c55e4722c
Yingjie-xue <66789700+Yingjie-xue@users.noreply.github.com>
2020-11-21 18:33:33 -0500

    Update resnet.py

--- resnet.py
+++ resnet.py
@@ -25,8 +25,8 @@ class  ResNet18(tf.keras.Model):
 
         self.batch_size = batch_size
         self.num_classes = num_classes
-    
-        # TODO: Initialize all hyperparameters
+        # Initialize all hyperparameters
+        self.learning_rate = 1e-3
         self.stride_all = 2
         # parameters for conv1
         self.conv1_num_outputs = 64
@@ -60,7 +60,8 @@ class  ResNet18(tf.keras.Model):
         self.conv4 =;
         self.conv5 = block();
         self.dense = tf.keras.layers.Dense(self.dense_output_size,activation='softmax')
-  
+        ## initialize optimizers
+        optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
     def call(self, inputs, is_testing=False):
         """
         Runs a forward pass on an input batch of images.
@@ -72,39 +73,30 @@ class  ResNet18(tf.keras.Model):
         # shape of input = (num_inputs (or batch_size), in_height, in_width, in_channels)
         # shape of filter = (filter_height, filter_width, in_channels, out_channels)
         # shape of strides = (batch_stride, height_stride, width_stride, channels_stride)
+        
         #conv layer 1
-        #print("inputs.shape",tf.shape(inputs))
-        conv_1 = tf.nn.conv2d(inputs,self.filter_conv_layer1, self.stride_layer1, self.padding)
-        #print("conv_1.shape",tf.shape(conv_1))
-        mean_1, variance_1 = tf.nn.moments(conv_1,axes = [0,1,2])
-        batch_norm_1 = tf.nn.batch_normalization(conv_1,mean_1,variance_1,offset=None, scale=None, variance_epsilon=1e-5)
-        #print("batch_norm_1.shape",tf.shape(batch_norm_1))
+        output_conv_1 = tf.nn.conv2d(inputs,self.filter_conv_layer1, self.stride_layer1, self.padding)
+        mean_1, variance_1 = tf.nn.moments( output_conv_1,axes = [0,1,2],keep_dims=False)
+        batch_norm_1 = tf.nn.batch_normalization(output_conv_1,mean_1,variance_1,offset=None, scale=None, variance_epsilon=1e-5)
         relu_1 = tf.nn.relu(batch_norm_1)
-        max_pool_1 = tf.nn.max_pool(relu_1,self.max_pooling_ksize_layer1,self.max_pooling_strides_layer1,padding = 'SAME')
-        #print("max_pool_1.shape",tf.shape(max_pool_1))
-        # conv layer 2
-        conv_2 = tf.nn.conv2d(max_pool_1,self.filter_conv_layer2, self.stride_layer2, self.padding)
-        mean_2, variance_2 = tf.nn.moments(conv_2,axes = [0,1,2])
-        batch_norm_2 = tf.nn.batch_normalization(conv_2,mean_2,variance_2,offset=None, scale=None, variance_epsilon=1e-5)
-        #print("batch_norm_2.shape",tf.shape(batch_norm_2))
-        relu_2 = tf.nn.relu(batch_norm_2)
-        max_pool_2 = tf.nn.max_pool(relu_2,self.max_pooling_ksize_layer2,self.max_pooling_strides_layer2,padding = 'SAME')
-        #print("max_pool_2.shape",tf.shape(max_pool_2))
-        # conv layer 3
-        if is_testing == True:
-           conv_3 = conv2d(max_pool_2,self.filter_conv_layer3, self.stride_layer3, self.padding)
-        else:
-            conv_3 = tf.nn.conv2d(max_pool_2,self.filter_conv_layer3, self.stride_layer3, self.padding)
-        mean_3, variance_3 = tf.nn.moments(conv_3,axes =[0,1,2])
-        batch_norm_3 = tf.nn.batch_normalization(conv_3,mean_3,variance_3,offset=None, scale=None, variance_epsilon=1e-5)
-        relu_3 = tf.nn.relu(batch_norm_3)
-        #print("relu_3.shape",tf.shape(relu_3))
-        logits_1 = tf.nn.relu(tf.matmul(tf.reshape(relu_3,(relu_3.numpy().shape[0],-1)),self.weights_dense_1)+self.bias_dense_1)
-        logits_1 = tf.nn.dropout(logits_1 ,0.3)
-        logits_2 = tf.nn.relu(tf.matmul(logits_1,self.weights_dense_2)+self.bias_dense_2)
-        logits_2 = tf.nn.dropout(logits_2,0.3)
-        logits_3 = tf.matmul(logits_2,self.weights_dense_3)+self.bias_dense_3
-        return logits_3
+        max_pool_1 = tf.nn.max_pool(relu_1, self.conv1_max_pool_size,self.stride_all = 2,padding = 'SAME')
+        # conv_2_x
+        output_conv_2 = self.conv2()
+        # conv_3_x
+        output_conv_3 = self.conv3()
+        # conv_4_x
+        output_conv_4 = self.conv4()
+        # conv_5_x
+        output_conv_5 = self.conv5()
+        # apply ave_pool
+        output_after_ave_pool = tf.nn.ave_pool(output_conv_5,self.ave_pool_size = 7)
+        # flatten it
+        after_flatten = tf.squeeze(output_after_ave_pool,[1,2])
+        ## input to dense
+        probs  = self.dense(after_flatten)
+        return probs
+        
+        
 
     def loss(self, logits, labels):
         """
@@ -137,7 +129,7 @@ class  ResNet18(tf.keras.Model):
         correct_predictions = tf.equal(tf.argmax(logits, 1), tf.argmax(labels, 1))
         return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
 
-def train(model, train_inputs, train_labels):
+def train(model, dir_name_train):
     '''
     Trains the model on all of the inputs and labels for one epoch. You should shuffle your inputs 
     and labels - ensure that they are shuffled in the same order using tf.gather.
@@ -150,39 +142,21 @@ def train(model, train_inputs, train_labels):
     shape (num_labels, num_classes)
     :return: Optionally list of losses per batch to use for visualize_loss
     '''
-    optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
-    # bacth number
-    batch_number = (np.ceil(np.divide(len(train_labels), model.batch_size))).astype(int)
-    range_tensor = tf.constant(np.arange(train_inputs.shape[0]))
-    # train in one loop
-    for j in range(10):
-        indices = tf.random.shuffle(range_tensor)
-        shuffled_inputs = tf.gather(train_inputs,indices)
-        shuffled_labels = tf.gather(train_labels,indices)
-        for i in range(batch_number):
-            if i < batch_number - 1:
-                X = shuffled_inputs[i * model.batch_size :(i+1) * model.batch_size]
-                Y = shuffled_labels[i * model.batch_size :(i+1) * model.batch_size]
-            else:
-                X = shuffled_inputs[i * model.batch_size :len(train_labels)]
-                Y = shuffled_labels[i * model.batch_size :len(train_labels)]
-        # Implement backprop:
-            with tf.GradientTape() as tape:
-                predictions = model.call(X) # this calls the call function conveniently
-                losses = model.loss(predictions,Y)
-      
-            #if i % 1000 == 0:
-               #train_acc = model.accuracy(model.call(train_inputs), train_labels)
-               #print("Accuracy on training set after {} training steps: {}".format(i, train_acc))
+    dir_path_train = dir_name_train  +'/*.jpg'
+    dataset = tf.data.Dataset.list_files(dir_path_train)
+    dataset.map(map_func=load_and_process_image,num_parallel_calls = 8)
+    dataset = dataset.batch(model.batch_size,drop_remainder=True)
+    dataset = dataset.prefecth(1)
     
-        # The keras Model class has the computed property trainable_variables to conveniently
-        # return all the trainable variables you'd want to adjust based on the gradients
-            gradients = tape.gradient(losses, model.trainable_variables)
-            optimizer.apply_gradients(zip(gradients, model.trainable_variables))
+   labels = get_labels ()
+    for i, batch in enumerate(dataset):
+        with tf.GradientTape() as tape:
+            predictions = model.call(batch) #
+            losses = model.loss(predictions,labels[i*model.batch_size,(i+1)*model.batch_size])
+        gradients = tape.gradient(losses, model.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, model.trainable_variables))
 
-    pass
-
-def test(model, test_inputs, test_labels):
+def test(model, dir_name_test):
     """
     Tests the model on the test inputs and labels. You should NOT randomly 
     flip images or do any extra preprocessing.
@@ -193,78 +167,18 @@ def test(model, test_inputs, test_labels):
     :return: test accuracy - this should be the average accuracy across
     all batches
     """
-
-    logits = model.call(test_inputs,is_testing = True)
-    accuracy = model.accuracy(logits,test_labels)
+    dir_path_test = dir_name_train  +'/*.jpg'
+    dataset = tf.data.Dataset.list_files(dir_path_test)
+    dataset.map(map_func=load_and_process_image,num_parallel_calls = 8)
+    dataset = dataset.batch(model.batch_size,drop_remainder=True)
+    dataset = dataset.prefecth(1)
+    test_labels =get_labels
+    total_accuracy = 
+    for i, bacth in enumerate(dataset):
+        logits = model.call(batch,is_testing = True)
+        accuracy = model.accuracy(logits,test_labels)
     print("test accuracy",accuracy)
     return accuracy
-
-
-def visualize_loss(losses): 
-    """
-    Uses Matplotlib to visualize the losses of our model.
-    :param losses: list of loss data stored from train. Can use the model's loss_list 
-    field 
-
-    NOTE: DO NOT EDIT
-
-    :return: doesn't return anything, a plot should pop-up 
-    """
-    x = [i for i in range(len(losses))]
-    plt.plot(x, losses)
-    plt.title('Loss per batch')
-    plt.xlabel('Batch')
-    plt.ylabel('Loss')
-    plt.show()  
-
-
-def visualize_results(image_inputs, probabilities, image_labels, first_label, second_label):
-    """
-    Uses Matplotlib to visualize the correct and incorrect results of our model.
-    :param image_inputs: image data from get_data(), limited to 50 images, shape (50, 32, 32, 3)
-    :param probabilities: the output of model.call(), shape (50, num_classes)
-    :param image_labels: the labels from get_data(), shape (50, num_classes)
-    :param first_label: the name of the first class, "cat"
-    :param second_label: the name of the second class, "dog"
-
-    NOTE: DO NOT EDIT
-
-    :return: doesn't return anything, two plots should pop-up, one for correct results,
-    one for incorrect results
-    """
-    # Helper function to plot images into 10 columns
-    def plotter(image_indices, label): 
-        nc = 10
-        nr = math.ceil(len(image_indices) / 10)
-        fig = plt.figure()
-        fig.suptitle("{} Examples\nPL = Predicted Label\nAL = Actual Label".format(label))
-        for i in range(len(image_indices)):
-            ind = image_indices[i]
-            ax = fig.add_subplot(nr, nc, i+1)
-            ax.imshow(image_inputs[ind], cmap="Greys")
-            pl = first_label if predicted_labels[ind] == 0.0 else second_label
-            al = first_label if np.argmax(
-                image_labels[ind], axis=0) == 0 else second_label
-            ax.set(title="PL: {}\nAL: {}".format(pl, al))
-            plt.setp(ax.get_xticklabels(), visible=False)
-            plt.setp(ax.get_yticklabels(), visible=False)
-            ax.tick_params(axis='both', which='both', length=0)
-        
-    predicted_labels = np.argmax(probabilities, axis=1)
-    num_images = image_inputs.shape[0]
-
-    # Separate correct and incorrect images
-    correct = []
-    incorrect = []
-    for i in range(num_images): 
-        if predicted_labels[i] == np.argmax(image_labels[i], axis=0): 
-            correct.append(i)
-        else: 
-            incorrect.append(i)
-
-    plotter(correct, 'Correct')
-    plotter(incorrect, 'Incorrect')
-    plt.show()
 
 
 def main():
@@ -281,14 +195,8 @@ def main():
     
     :return: None
     '''
-    train_inputs_file_path = 'data/train'
-    test_inputs_file_path = 'data/test'
-    train_inputs, train_labels = get_data(train_inputs_file_path,3,5)
-    test_inputs, test_labels = get_data(test_inputs_file_path, 3,5)
-    model = Model()
-    train(model,train_inputs,train_labels)
-    test(model,test_inputs,test_labels)
-    
+    X_train, y_train, X_test, y_test = load_cifar10_dataset(shape=(-1, 32, 32, 3), plotable=False)
+        
     return
 
 
