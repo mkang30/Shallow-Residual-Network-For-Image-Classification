f0484bd058643843200d6e9802b5940d9d7c3b2b
Yingjie-xue <66789700+Yingjie-xue@users.noreply.github.com>
2020-11-24 17:53:22 -0500

    accuracy = 0.5 based on the current code

--- preprocess.py
+++ preprocess.py
@@ -1,7 +1,47 @@
 import pickle
 import numpy as np
+<<<<<<< Updated upstream
 import tensorflow as tf
 import os
+=======
+import tensorflow_datasets as tfds
+
+"""
+This module is responsible for the preprocessing of the image datasets
+"""
+
+"""
+This function loads the CIFAR10 datasets
+return:
+train_images - numpy array of the shape (50000,32,32,3)
+train_labels - numpy array of the shape (50000,)
+test_images - numpy array of the shape (10000,32,32,3)
+test_labels - numpy array of the shape (10000,)
+"""
+def get_data():
+    data = tfds.load("cifar10")
+    train, test = data["train"],data["test"]
+
+    train_iter = train.as_numpy_iterator()
+    test_iter = test.as_numpy_iterator()
+    train_images = []
+    train_labels = []
+    test_images = []
+    test_labels = []
+    for datum in train_iter:
+        train_images.append(datum["image"])
+        train_labels.append(datum["label"])
+    for datum in test_iter:
+        test_images.append(datum["image"])
+        test_labels.append(datum["label"])
+    norm_factor = 1/255
+    train_images = np.array(train_images,dtype=np.float32)*norm_factor
+    train_images = train_images
+    train_labels = np.array(train_labels)
+    test_images = np.array(test_images,dtype=np.float32)*norm_factor
+    test_images = test_images
+    test_labels = np.array(test_labels)
+>>>>>>> Stashed changes
 
 def get_data(file_path, num_classes, classes):
 	#Given a file path and num_classes, classes which is a list of  indexes indicating the classes 
